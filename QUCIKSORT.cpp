510e79545ce9f122d7f544db63d6e01d18533ac4
Saanvi0804 <saanvi.082005@gmail.com>
2024-12-11 20:33:38 +0530

    Update QUCIKSORT.cpp

--- QUCIKSORT.cpp
+++ QUCIKSORT.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 using namespace std;
-void swap(int& a, int& b) {
+
+void swap(int& a, int& b) 
+{
     int t = a;
     a = b;
     b = t;
 }
 
-int Partition(int A[], int l, int r) {
+int Partition(int A[], int l, int r) 
+{
     int p = A[l];
     int i = l;
     int j = r + 1;
 
-    do
-    {
+    do {
         do
-          i++;
+            i++;
         while (A[i] < p && i <= r);
 
         do
-          j--;
+            j--;
         while (A[j] > p && j >= l);
 
         if (i >= j) break;
         swap(A[i], A[j]);
-    }
-    while (true);
+    } while (true);
 
     swap(A[l], A[j]);
     return j;
 }
 
-void QuickSort(int A[], int l, int r) {
+void QuickSort(int A[], int l, int r) 
+{
     if (l < r) {
         int s = Partition(A, l, r);
         QuickSort(A, l, s - 1);
@@ -38,25 +40,30 @@ void QuickSort(int A[], int l, int r) {
     }
 }
 
-
 void PrintArray(int A[], int size)
 {
     for (int i = 0; i < size; i++) {
-        cout << A[i] << " ";
+        cout<<A[i] <<" ";
     }
-    cout << endl;
+    cout<<endl;
 }
 
-int main() {
-    int A[] = {78, 67, 70, 90, 16, 25};
-    int n = sizeof(A) / sizeof(A[0]);
+int main()
+{
+    int A[100];
+    cout<<"Enter the number of elements in the array"<<endl;
+    cin>>n;
+    cout<<"Enter "<<n<<" elements: ";
+    for (int i = 0; i < n; i++) {
+        cin>>A[i];
+    }
 
-    cout << "Unsorted array: ";
+    cout<<"Unsorted array: ";
     PrintArray(A, n);
 
     QuickSort(A, 0, n - 1);
 
-    cout << "Sorted array: ";
+    cout<<"Sorted array: ";
     PrintArray(A, n);
 
     return 0;
