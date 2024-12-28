d3cd593919e8aa520b1fccb1513542e8e84b08f9
Saanvi0804 <saanvi.082005@gmail.com>
2024-12-28 23:43:42 +0530

    Update heap.cpp

--- wheat_heap.cpp
+++ wheat_heap.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-void heapify(int a[], int n) {
+void heapify(int a[], int n)
+{
     int k, heap, v, j;
-    for (int i = n / 2; i >= 1; i--) {
+    for (int i = n / 2; i >= 1; i--)
+    {
         k = i;
         v = a[k];
         heap = 0;
-        while (heap != 1 && 2 * k <= n) {
+        while (heap != 1 && 2 * k <= n)
+        {
             j = 2 * k;
-            if (j < n) {
+            if (j < n)
+            {
                 if (a[j] < a[j + 1])
                     j = j + 1;
             }
             if (v >= a[j])
                 heap = 1;
-            else {
+            else
+            {
                 a[k] = a[j];
                 k = j;
             }
@@ -25,33 +29,18 @@ void heapify(int a[], int n) {
     }
 }
 
-int main() {
-    int a[100]; // Start indexing from 1
-    int n = 0;
-
-    ifstream file("wheat_qua.txt");
-
-    if (!file) {
-        cout << "File Not Found\n";
-        return 1;
-    }
-
-    string line;
-    cout << "Reading data from file:\n";
-    while (getline(file, line)) {
-        int id, quality;
-        sscanf(line.c_str(), "%d, %d", &id, &quality);
-        cout << "ID: " << id << ", Quality: " << quality << endl; // Debug print
-        a[++n] = quality; // Add quality value to heap array
-    }
-    file.close();
-
+int main()
+{
+    int a[100], n;
+    cout << "Enter size:" << endl;
+    cin >> n;
+    cout << "Enter elements:" << endl;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
     heapify(a, n);
-
     cout << "Max Heap:" << endl;
     for (int i = 1; i <= n; i++)
         cout << a[i] << " ";
     cout << endl;
-
     return 0;
 }
